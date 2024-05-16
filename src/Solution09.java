37750797bbc6a22b0af4fdc6be8a3ef33bb2a599
fwangjuwon <fwangjuwon@naver.com>
2024-05-16 22:16:22 +0900

    프로그래머스 기초_2일차

---
+++ src/Solution09.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution09 {
+    public static void main(String[] args) {
+        // 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해
+        // 보세요.
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a % 2 == 0) {
+            System.out.println(a + " is even");
+        } else {
+            System.out.println(a + " is odd");
+        }
+    }
+}
